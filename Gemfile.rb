d834e40d2a12742cdf3203afbe79cbe34df14325
YuraMelesh <yuramelesh@gmail.com>
2016-03-28 19:06:21 +0300

    adding Gemfile

---
+++ Gemfile.rb
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'json'
+gem 'net/http'
+gem 'octokit', '~> 4.0'
+gem 'sinatra'
+gem 'haml'
+gem 'sqlite3', '~> 1.3', '>= 1.3.11'
+gem 'activerecord', '~> 4.2', '>= 4.2.6'
+gem 'curb'
+gem 'typhoeus'
