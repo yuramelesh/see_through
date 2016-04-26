54486fd84f7e583a6e2a28430426e535894da463
YuraMelesh <yuramelesh@gmail.com>
2016-04-26 17:45:50 +0300

    init

---
+++ ops/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./deploy.sh  ec2-52-38-225-163.us-west-2.compute.amazonaws.com ubuntu ~/.ssh/see_through_yuramelesh ../../see_through
+
+#./copy_project_to_server.sh ec2-52-38-225-163.us-west-2.compute.amazonaws.com ubuntu /home/melesh/.ssh/see_through_yuramelesh /home/melesh/RubymineProjects/see_through/
